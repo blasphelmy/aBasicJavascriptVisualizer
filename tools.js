41a6fe2312504d7327b6c25bb4ac8d0a6a0524bf
blasphemly <97411482+blasphemly@users.noreply.github.com>
2022-03-04 15:18:44 -0800

    dd

--- tools.js
+++ tools.js
@@ -1,38 +1,40 @@
-function breakIntoComponents(inputString){ //this needs more work
-    //first, we must first remove all line breaks and whitespace. we also assume that user written with semi colons...
-    inputString = inputString.replace(/\n/g, '');
-    inputString = inputString.split(/(;)/g);
-    // inputString.match(/[^\;]+\;?|\;/g);
-    var outputArray = new Array();
+function getInputString() {}
 
-    for(x = 0; x < inputString.length; x++){
-        var temp = inputString[x].split(/([}{])/g);
-        for(index = 0; index < temp.length; index++){
-            outputArray.push(temp[index]);
-        }
+function breakIntoComponents(inputString) {
+  //this needs more work
+  //first, we must first remove all line breaks and whitespace. we also assume that user written with semi colons...
+  inputString = inputString.replace(/\n/g, "");
+  inputString = inputString.split(/(;)/g);
+  // inputString.match(/[^\;]+\;?|\;/g);
+  var outputArray = new Array();
+
+  for (x = 0; x < inputString.length; x++) {
+    var temp = inputString[x].split(/([}{])/g);
+    for (index = 0; index < temp.length; index++) {
+      outputArray.push(temp[index]);
     }
-    //we need to do all of this because im terrible with regex.
-    outputArray = trimStringInArray(outputArray);
-    outputArray = removeEmptyIndices(outputArray);
-    outputArray = combineSemiColonsWithPreviousLines(outputArray);
-    outputArray = splitIntoTokens(outputArray);
-    outputArray = removeEmptyIndices(outputArray);
-    return outputArray;
+  }
+  //we need to do all of this because im terrible with regex.
+  outputArray = trimStringInArray(outputArray);
+  outputArray = removeEmptyIndices(outputArray);
+  outputArray = combineSemiColonsWithPreviousLines(outputArray);
+  outputArray = splitIntoTokens(outputArray);
+  outputArray = removeEmptyIndices(outputArray);
+  return outputArray;
 }
-function splitIntoTokens(array){
-    var newArray = new Array();
+function splitIntoTokens(array) {
+  var newArray = new Array();
 
+<<<<<<< HEAD
     for(var index = 0; index< array.length; index++){
-        if(isFunction(array[index])){
-            var temp = array[index].split(/(function)/gm);
-            console.log("temp",temp);
+        if(array[index].match(/(function[ ]*)/gm)){
+            var temp = array[index].split(/(function[ ]*)/gm);
             for(var x = 0; x < temp.length; x++){
                 newArray.push(temp[x]);
             }
         }
-        if(array[index].match(/(var[ ]*)/gm)){
+        else if(array[index].match(/(var[ ]*)/gm)){
             var temp = array[index].split(/(var)[ ]/gm);
-            console.log("temp",temp);
             for(var x = 0; x < temp.length; x++){
                 newArray.push(temp[x]);
             }
@@ -52,39 +54,72 @@ function combineSemiColonsWithPreviousLines(array){
         }else if(!array[index+1].match(";")){
             newArray.push(array[index]);
         }
+=======
+  for (var index = 0; index < array.length; index++) {
+    if (isFunction(array[index])) {
+      var temp = array[index].split(/(function)/gm);
+      console.log("temp", temp);
+      for (var x = 0; x < temp.length; x++) {
+        newArray.push(temp[x]);
+      }
     }
-    return newArray;
+    if (array[index].match(/(var[ ]*)/gm)) {
+      var temp = array[index].split(/(var)[ ]/gm);
+      console.log("temp", temp);
+      for (var x = 0; x < temp.length; x++) {
+        newArray.push(temp[x]);
+      }
+    } else {
+      newArray.push(array[index]);
+>>>>>>> 19c466bf717d14108efcab56a9476f66aa9d00e8
+    }
+  }
+  return newArray;
 }
-function trimStringInArray(array){
-    for(index = 0; index < array.length; index++){
-        array[index].trim(); 
+function combineSemiColonsWithPreviousLines(array) {
+  var newArray = new Array();
+  for (index = 0; index < array.length; index++) {
+    if (array[index].match(";")) {
+      var temp = array[index - 1] + array[index];
+      newArray.push(temp);
+    } else if (!array[index + 1].match(";")) {
+      newArray.push(array[index]);
     }
-    return array;
+  }
+  return newArray;
 }
-function removeEmptyIndices(array){ //clean array if nothing is detected on indexes
-    var newArray = new Array();
-    for(index = 0; index < array.length; index++){
-        if(!(array[index].trim().length === 0)){
-            newArray.push(array[index]);
-        }
-    }
-    return newArray;
+function trimStringInArray(array) {
+  for (index = 0; index < array.length; index++) {
+    array[index].trim();
+  }
+  return array;
 }
-function findEOFLine(array, start){ //raw string -> breakIntoComponents() first. start is the index of the array where a function declartion is detected.
-    var count = 0;
-    var endLine = -1;
-    for(index = start; index < array.length; index++){
-        if(array[index].search("{") > -1){
-            count = count + 1;
-        }
-        if(array[index].search("}") > -1){
-            count = count - 1;
-        }
-        if(count === 0 && array[index].search("}") > -1){
-            endLine = index;
-            break;
-        }
-        // console.log(count);
+function removeEmptyIndices(array) {
+  //clean array if nothing is detected on indexes
+  var newArray = new Array();
+  for (index = 0; index < array.length; index++) {
+    if (!(array[index].trim().length === 0)) {
+      newArray.push(array[index]);
     }
-    return endLine;
+  }
+  return newArray;
+}
+function findEOFLine(array, start) {
+  //raw string -> breakIntoComponents() first. start is the index of the array where a function declartion is detected.
+  var count = 0;
+  var endLine = -1;
+  for (index = start; index < array.length; index++) {
+    if (array[index].search("{") > -1) {
+      count = count + 1;
+    }
+    if (array[index].search("}") > -1) {
+      count = count - 1;
+    }
+    if (count === 0 && array[index].search("}") > -1) {
+      endLine = index;
+      break;
+    }
+    // console.log(count);
+  }
+  return endLine;
 }
