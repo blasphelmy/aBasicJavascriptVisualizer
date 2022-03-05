1e3ec09a8857d2d929100d55b664b9ff9ded11d0
blasphemly <97411482+blasphemly@users.noreply.github.com>
2022-03-04 19:56:40 -0800

    asd

--- tools.js
+++ tools.js
@@ -2,7 +2,7 @@ function getInputString() {}
 
 function breakIntoComponents(inputString) {
   //this needs more work
-  //first, we must first remove all line breaks and whitespace. we also assume that user written with semi colons...
+  //first, we must first remove all line breaks and whitespace. we also assume that user writes javascript with the use of semi colons...
   inputString = inputString.replace(/\n/g, "");
   inputString = inputString.split(/(;)/g);
   // inputString.match(/[^\;]+\;?|\;/g);
@@ -22,30 +22,31 @@ function breakIntoComponents(inputString) {
   outputArray = removeEmptyIndices(outputArray);
   return outputArray;
 }
-function splitIntoTokens(array) {
+function splitIntoTokens(array) { //ONLY USE THIS AFTER INPUT STRING IS COMPLETELY BROKEN DOWN AND CLEANSED.
   var newArray = new Array();
-
   for (var index = 0; index < array.length; index++) {
+    //FOR FUNCTION DECLARATIONS
     if (array[index].match(/(function[ ])/gm)) {
       var temp = array[index].split(/(function[ ])/gm);
       for (var x = 0; x < temp.length; x++) {
         newArray.push(temp[x]);
       }
     }
+    //FOR VARIABLE DECLARATIONS
     else if (array[index].match(/(var[ ]*)/gm)) {
       var temp = array[index].split(/(var)[ ]/gm);
       for (var x = 0; x < temp.length; x++) {
         newArray.push(temp[x]);
       }
-    } else {
+    } else { //EVERYTHING ELSE SHOULD JUST BE REGULAR STATEMENTS, ei variable declare / redeclaration, function calls,  in theory this method should allow for the addition of if's loops etc...
       newArray.push(array[index]);
     }
   }
   return newArray;
 }
-function combineSemiColonsWithPreviousLines(array) {
-  var newArray = new Array();
-  for (index = 0; index < array.length; index++) {
+function combineSemiColonsWithPreviousLines(array) { //cause i cant figure out how to split by semi colons on the 
+  var newArray = new Array();                       //same line. whelps
+  for (index = 0; index < array.length; index++) { 
     if (array[index].match(";")) {
       var temp = array[index - 1] + array[index];
       newArray.push(temp);
@@ -84,6 +85,32 @@ function findEOFLine(array, start) {
     }
     if (count === 0 && array[index].search("}") > -1) {
       endLine = index;
+      break;
+    }
+    // console.log(count);
+  }
+  return endLine;
+}
+function returnFunctionStack(array, start, end){ //returns a stack of statements as an array
+  var newArray = new Array();
+  for(index = start; index <= end; index++){
+    newArray.push(array[index]);
+  }
+  return newArray;
+}
+function findEOFLine(array, start) {
+  //raw string -> breakIntoComponents() first. start is the index of the array where a function declartion is detected.
+  var count = 0;
+  var endLine = -1;
+  for (var x = start; x < array.length; x++) {
+    if (array[x].search("{") > -1) {
+      count = count + 1;
+    }
+    if (array[x].search("}") > -1) {
+      count = count - 1;
+    }
+    if (count === 0 && array[x].search("}") > -1) {
+      endLine = x;
       break;
     }
     // console.log(count);
